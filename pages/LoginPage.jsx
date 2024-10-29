1fd8e6d48016a1639d41e71393b77391b811154d
Crispin Manda <crispinmanda06@gmail.com>
2024-10-29 13:17:05 +0300

    feat(photo-detail): implement photo detail view and title editing
    - Create `PhotoDetail` component to display and edit photo details
    - Fetch photo data by ID and set initial title state
    - Implement title update functionality with API call on save
    - Utilize React Bootstrap for form styling and layout

---
+++ pages/LoginPage.jsx
@@ -0,0 +1,75 @@
+import './LoginPage.css';
+import { Container,Row, Col } from 'react-bootstrap';
+import { useGoogleLogin } from "@react-oauth/google";
+import { useAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+function LoginPage() {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log("Login successful:", tokenResponse);
+
+      localStorage.setItem('token', tokenResponse.access_token);
+
+      try {
+        const userInfoResponse = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
+          headers: {
+            Authorization: `Bearer ${tokenResponse.access_token}`,
+          },
+        });
+        const userInfo = await userInfoResponse.json();
+        console.log('User Info:', userInfo);
+
+        login(userInfo);
+
+        console.log('Redirecting to /home');
+        navigate('/home');
+      } catch (error) {
+        console.error('Error fetching user info:', error);
+      }
+    },
+    onError: (error) => {
+      console.error('Login Failed:', error);
+    },
+  });
+
+  return (
+  
+<Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+  <Col className="d-flex justify-content-center">
+    <div className="card shadow mb-3"
+      style={{
+        width: '30rem',
+        height:'450px',
+        padding: '20px',
+        boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)', 
+      }} >
+      <div className="card-header text-center"><h4>Welcome to Snapspace</h4></div>
+      <div className="card-body text-dark">
+        <p className="card-text text-center mt-3">Sign in to continue.</p>
+        
+        <Row className="d-flex justify-content-center">
+          <button
+            onClick={() => {
+              console.log("Attempting to login...");
+              googleLogin();
+            }}
+            type="button"
+            className="login-with-google-btn mt-5"
+            style={{ width: '100%' }}
+          >
+            Continue with Google
+          </button>
+        </Row>
+      </div>
+    </div>
+  </Col>
+</Container>
+
+);
+}
+
+export default LoginPage;
