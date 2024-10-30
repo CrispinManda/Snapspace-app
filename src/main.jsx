834ccc3c9696fb9d5002da523c94cf0177b9f8ba
Crispin Manda <crispinmanda06@gmail.com>
2024-10-31 00:21:33 +0300

    chore(env): add/update environment variables for application configuration
    - Added `VITE_GOOGLE_CLIENT_ID` for Google OAuth authentication
    - Updated documentation in `.env` file for environment variable usage
    - Ensured sensitive keys are included in `.gitignore`

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-
-
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId="1009789889151-fclkfodpu8b196uvii57o0k6jim1a5m7.apps.googleusercontent.com">
-    <App />
-    </GoogleOAuthProvider>;
-  </StrictMode>,
-)
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
