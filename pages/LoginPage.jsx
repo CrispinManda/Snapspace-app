22246b4b869e5b114a0b5ce2caa4ae6060b6e672
Crispin Manda <crispinmanda06@gmail.com>
2024-10-30 16:10:38 +0300

    style:added styles to login card

--- pages/LoginPage.jsx
+++ pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import './LoginPage.css';
-import { Container,Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { useGoogleLogin } from "@react-oauth/google";
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
@@ -37,39 +37,40 @@ function LoginPage() {
   });
 
   return (
-  
-<Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
-  <Col className="d-flex justify-content-center">
-    <div className="card shadow mb-3"
-      style={{
-        width: '30rem',
-        height:'450px',
-        padding: '20px',
-        boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)', 
-      }} >
-      <div className="card-header text-center"><h4>Welcome to Snapspace</h4></div>
-      <div className="card-body text-dark">
-        <p className="card-text text-center mt-3">Sign in to continue.</p>
-        
-        <Row className="d-flex justify-content-center">
-          <button
-            onClick={() => {
-              console.log("Attempting to login...");
-              googleLogin();
-            }}
-            type="button"
-            className="login-with-google-btn mt-5"
-            style={{ width: '100%' }}
-          >
-            Continue with Google
-          </button>
-        </Row>
-      </div>
-    </div>
-  </Col>
-</Container>
-
-);
+    <Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <Col xs={12} md={8} lg={6} className="d-flex justify-content-center">
+        <div
+          className="card shadow mb-3"
+          style={{
+            width: '100%', // Make the card take full width of the column
+            maxWidth: '30rem', // Set a max-width for larger screens
+            height: '450px',
+            padding: '20px',
+            boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.2)',
+          }}
+        >
+          <div className="card-header text-center"><h4>Welcome to Snapspace</h4></div>
+          <div className="card-body text-dark">
+            <p className="card-text text-center mt-3">Sign in to continue.</p>
+            
+            <Row className="d-flex justify-content-center">
+              <button
+                onClick={() => {
+                  console.log("Attempting to login...");
+                  googleLogin();
+                }}
+                type="button"
+                className="login-with-google-btn mt-5"
+                style={{ width: '100%' }}
+              >
+                Continue with Google
+              </button>
+            </Row>
+          </div>
+        </div>
+      </Col>
+    </Container>
+  );
 }
 
 export default LoginPage;
